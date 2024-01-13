b147f9a9f0d75b74eefe793bfc05b7c2e54eb94e
anastahri44 <131082849+anastahri44@users.noreply.github.com>
2024-01-13 16:53:27 +0100

    TP1

--- exercice3/src/main/java/org/masterS1/Main.java
+++ exercice3/src/main/java/org/masterS1/Main.java
@@ -5,6 +5,8 @@ import org.masterS1.entities.Moto;
 import org.masterS1.entities.Vehicule;
 import org.masterS1.entities.Voiture;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,8 +15,8 @@ public class Main {
 //        compris le prix, et en appelant la méthode emettreSon() pour faire émettre le son spécifique
 //        de chaque véhicule. Enfin, affichez toutes les informations, y compris les attributs
 //        supplémentaires et le prix, avec la méthode afficherInformations().
-
-        Vehicule vehiculeVoiture = new Voiture("Audi",320000,"2023");
+// first method
+        /*Vehicule vehiculeVoiture = new Voiture("Audi",320000,"2023");
         Vehicule vehiculeMoto = new Moto("Moto",12000,"Marque");
         Vehicule vehiculeAvion = new Avion("Avion",4000000,"Comp1","5000");
         vehiculeVoiture.emettreSon();
@@ -22,6 +24,16 @@ public class Main {
         vehiculeAvion.emettreSon();
         vehiculeVoiture.afficherInformations();
         vehiculeMoto.afficherInformations();
-        vehiculeAvion.afficherInformations();
+        vehiculeAvion.afficherInformations();*/
+
+        //polymorphism method
+        Vehicule[] vehicules= new Vehicule[3];
+        vehicules[0]  = new Voiture("Audi",320000,"2023");
+        vehicules[1]  = new Moto("Moto",12000,"Marque");
+        vehicules[2]  = new Avion("Avion",4000000,"Comp1","5000");
+        for (Vehicule vehicule: vehicules){
+            vehicule.emettreSon();
+            vehicule.afficherInformations();
+        }
     }
 }
